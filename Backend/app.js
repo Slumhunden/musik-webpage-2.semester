fbce53d4dcadeea421299058bb678e2c8518ff83
Slumhunden <kroghbastin@gmail.com>
2023-09-05 10:14:38 +0200

    finished backend and added ids to json file

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,54 @@
-import express from "express"
-
+import express from "express";
+import cors from "cors";
+import fs from "fs/promises";
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
 const port = 1919;
 
-app.listen(port, () =>{
-    console.log(`App is running on http://localhost:${port}`)
-})
+app.listen(port, () => {
+	console.log(`App is running on http://localhost:${port}`);
+});
+app.get("/artists", async (req, res) => {
+	const data = await fs.readFile("./Data/artists.json");
+	const artists = JSON.parse(data);
+	console.log(artists);
+	res.json(artists);
+});
+app.post("/artists", async (req, res) => {
+	const data = await fs.readFile("./Backend/Data/artists.json");
+	const artists = JSON.parse(data);
+	const newArtist = req.body;
+	newArtist.id = new Date().getTime();
+	artists.push(newArtist);
+	fs.writeFile("./Backend/Data/artists.json", JSON.stringify(artists));
+	res.json(artists);
+});
+
+app.put("/artists/:id", async (req, res) => {
+	const id = Number(req.params.id);
+	const data = await fs.readFile("./Backend/Data/artists.json");
+	const artists = JSON.parse(data);
+	let updateArtist = artists.find((artist) => artist.id === id);
+	const body = req.body;
+	updateArtist.name = body.name;
+	updateArtist.birthdate = body.birthdate;
+	updateArtist.genres = body.genres;
+	updateArtist.activeSince = body.activeSince;
+	updateArtist.labels = body.labels;
+	updateArtist.website = body.website;
+	updateArtist.image = body.image;
+	updateArtist.shortDescription = body.shortDescription;
+	fs.writeFile("./Backend/Data/artists.json", JSON.stringify(artists));
+	res.json(artists);
+});
+app.delete("/artists/:id", async (req, res) => {
+	const id = Number(req.params.id);
+	const data = await fs.readFile("/.Backend/Data/artists.json");
+	const artists = JSON.parse(data);
+	let deleteArtist = artists.filter((artist) => artist.id !== id);
+	fs.writeFile("./Backend/Data/artists.json", JSON.stringify(deleteArtist));
+	res.json(artists);
+});
--- Frontend/frontend.js
+++ Frontend/frontend.js
@@ -43,5 +43,5 @@ function displayArtist(artist) {
 }
 
 
-}
+
 
